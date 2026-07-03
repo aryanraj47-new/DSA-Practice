7af936dcd719070d571c41b4b06234e1ccc4ce6e
aryanraj47-new <aryanraj6647@gmail.com>
2026-07-03 14:06:34 +0530

    Time: 0 ms (100%), Space: 44.6 MB (68.64%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int piv = -1;
+        int n = nums.length;
+
+        
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                piv = i;
+                break;
+            }
+        }
+
+        
+        if (piv == -1) {
+            reverse(nums, 0, n - 1);
+            return;
+        }
+
+        
+        int nextg = -1;
+        for (int i = n - 1; i > piv; i--) {
+            if (nums[i] > nums[piv]) {
+                nextg = i;
+                break;
+            }
+        }
+
+        
+        swap(nums, piv, nextg);
+        reverse(nums, piv + 1, n - 1);
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
