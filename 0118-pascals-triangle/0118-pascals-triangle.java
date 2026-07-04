b4f53970c5a5ecf429c928413e354c1269f76571
aryanraj47-new <aryanraj6647@gmail.com>
2026-07-04 23:27:27 +0530

    Time: 1 ms (94.03%), Space: 43.7 MB (15.63%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,34 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> arr = new ArrayList<>();
+        List<List<Integer>> triangle = new ArrayList<>();
         
         
-        if (numRows == 0) return arr;
-        
-        
-        arr.add(new ArrayList<>(List.of(1)));
-        if (numRows == 1) return arr;
-        
-        
-        arr.add(new ArrayList<>(List.of(1, 1)));
-        
-        
-        for (int i = 2; i < numRows; i++) {
-            List<Integer> row = new ArrayList<>();
-            
-            
-            row.add(1); 
-            
-            
-            for (int j = 1; j < i; j++) {
-                row.add(arr.get(i - 1).get(j - 1) + arr.get(i - 1).get(j));
-            }
-            
-            
-            row.add(1); 
-            
-            arr.add(row);
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = generateRowUsingNCr(i);
+            triangle.add(row);
         }
-        return arr;
+        
+        return triangle;
+    }
+
+    
+    public List<Integer> generateRowUsingNCr(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        long val = 1; 
+        
+        for (int r = 0; r <= rowIndex; r++) {
+            row.add((int) val);
+            
+            
+            val = val * (rowIndex - r) / (r + 1);
+        }
+        
+        return row;
     }
 }
