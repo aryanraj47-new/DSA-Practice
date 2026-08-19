e65ab1acf1e5673867ab4b676664d9125e74a24d
aryanraj47-new <aryanraj6647@gmail.com>
2026-08-19 22:37:22 +0530

    Time: 0 ms (100%), Space: 44.6 MB (40.72%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -4,6 +4,7 @@ class Solution {
         int s=0,e=n-1;
         while(s<=e){
             int mid=s+(e-s)/2;
+            //no. of +ve integers missing before mid= arr[mid]-(mid+1)
             if(arr[mid]-(mid+1)<k){
                 s=mid+1;
             }
@@ -11,6 +12,11 @@ class Solution {
                 e=mid-1;
             }
         }
+        //missing nos. before end index=     arr[e]-e-1
+        //remaining missing nos. for k =     k-(arr[e]-e-1)
+        //kth missing no.=      arr[e]+remaining missing no.
+        //               =      k+(e+1)
+        //               =      k+s
         return s+k;
     }
 }
